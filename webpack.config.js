67eea4e9b26d51759235a2ac31896272697bf125
Andrey Sunsin <andreysunone@gmail.com>
2023-05-02 22:21:24 +0500

    самая базовая конфигурация проекта

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+// webpack.config.js
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: {
+      main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+],
+}
