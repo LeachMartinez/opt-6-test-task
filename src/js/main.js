e0a54a9ee759e39de9fdd3ca9c508e1c7ca3c7da
Andrey Sunsin <andreysunone@gmail.com>
2023-05-03 23:43:45 +0500

    некоторый функционал для таблицы

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 import Table from "./table";
+
+const table = new Table($("#myTable"));
+
+$("#addRow").on("click", e => {
+  table.addRow([
+    createInput("itemName", "Введите наименование единицы"),
+    createInput("price", "Введите цену"),
+  
+  ]);
+});
+
+const createInput = (id, placeholder = "Введите что-нибудь") => {
+  return `<input type="text" id="${id}" placeholder="${placeholder}">`
+}
--- src/js/table.js
+++ src/js/table.js
@@ -1,15 +1,37 @@
 import DataTable from 'datatables.net-dt';
-
+import 'datatables.net-rowreorder';
 class Table {
-  constructor(tableNodeId, config) {
-    this.dataTable = new DataTable( tableNodeId, config);
+
+  constructor(tableNodeId) {
+    this.dataTable = new DataTable( tableNodeId, {
+    "bPaginate": false,
+    "bInfo": false,
+    "searching": false,
+    rowReorder: {
+      selector: '.rowId'
+    },
+    columnDefs: [{
+      targets: 0,
+      visible: false
+    }]
+   });
   }
 
-  addRow() {
+  count = 1;
+
+  addRow(data) {
+    this.dataTable.row.add([this.#createIdBlock(), this.#createIdBlock(),...data]).draw(false);
+    this.count++;
   }
 
-  removeRow() {
+  removeRow($row) {
+    this.dataTable.row($row).remove().draw();
+  }
 
+  #createIdBlock() {
+    return `
+      <div class="rowId">${this.count}</div>
+    `
   }
 }
 
